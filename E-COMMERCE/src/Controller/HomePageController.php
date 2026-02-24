7520f85dd3b5e064b11b07a8e9ab9be1e8c03825
Robinhood2red <louisboutetcamille@gmail.com>
2026-02-24 14:57:52 +0100

    condition

---
+++ E-COMMERCE/src/Controller/HomePageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): Response
+    {
+        $nomstudents=['Jérmie', 'Léa', 'Pierick', 'Davy'];
+        $age =17;
+
+        return $this->render('home_page/index.html.twig', [
+            // 'controller_name' => 'HomePageController',
+            'lesNoms' => $nomstudents,
+            'age' => $age
+        ]);
+    }
+}
+// {
+//     #[Route('/', name: 'app_home_page')]
+//     public function index(): Response
+//     {
+//         return $this->render('home_page/index.html.twig', [
+//             'controller_name' => 'HomePageController',
+//         ]);
+//     }
+// }
