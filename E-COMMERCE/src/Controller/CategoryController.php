24abdbbcb2bad3386611042a7f866d948df5bd7e
louisr <louisboutetcamille@gmail.com>
2026-02-25 15:57:40 +0100

    Add category

---
+++ E-COMMERCE/src/Controller/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Form\CategoryFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Dom\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function addCategory(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $category = new Categorie();
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+
+        return $this->render('category/newCategory.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
