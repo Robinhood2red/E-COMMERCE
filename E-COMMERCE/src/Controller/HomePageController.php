063e3dff784b3c2f404317f963216012161e4e26
louisr <louisboutetcamille@gmail.com>
2026-03-04 16:19:51 +0100

    shop panier et coup de coeur

--- E-COMMERCE/src/Controller/HomePageController.php
+++ E-COMMERCE/src/Controller/HomePageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -22,11 +24,19 @@ final class HomePageController extends AbstractController
 //     }
 // }
 {
-    #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    #[Route('/', name: 'app_home_page', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'products'=>$productRepository->findAll()
+        ]);
+    }
+
+#[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
+    public function showProduct(Product $product): Response 
+    {
+        return $this->render('home_page/show.html.twig', [ 
+            'product' => $product
         ]);
     }
 }
