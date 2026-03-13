7e47952972c60af7ff417046cf82a67295965948
Robinhood2red <louisboutetcamille@gmail.com>
2026-03-13 11:49:51 +0100

    section

--- E-COMMERCE/src/Controller/HomePageController.php
+++ E-COMMERCE/src/Controller/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\CategorieRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,19 +25,25 @@ final class HomePageController extends AbstractController
 //     }
 // }
 {
-    #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+#[Route('/', name: 'app_home_page', methods: ['GET'])]
+    public function index(ProductRepository $productRepository, CategorieRepository $categorieRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
-            'products'=>$productRepository->findAll()
+            'products' => $productRepository->findAll(),
+            'categories' => $categorieRepository->findAll()
         ]);
     }
 
-#[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
-    public function showProduct(Product $product): Response 
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
+    public function showProduct(Product $product, ProductRepository $productRepository, CategorieRepository $categorieRepository): Response 
     {
+        // On récupère les 5 derniers produits par ID décroissant
+        $lastProductsAdd = $productRepository->findBy([], ['id' => 'DESC'], 5); 
+
         return $this->render('home_page/show.html.twig', [ 
-            'product' => $product
+            'product' => $product,
+            'products' => $lastProductsAdd,
+            'categories' => $categorieRepository->findAll()
         ]);
     }
 }
