04aee4a4e821f0b05e07979267857b612dce6ffd
Robinhood2red <louisboutetcamille@gmail.com>
2026-03-16 09:00:58 +0100

    filter

--- E-COMMERCE/src/Controller/HomePageController.php
+++ E-COMMERCE/src/Controller/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\AlphaCampRepository;
 use App\Repository\CategorieRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,20 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
-// {
-//     #[Route('/', name: 'app_home_page')]
-//     public function index(): Response
-//     {
-//         $nomstudents=['Jérmie', 'Léa', 'Pierick', 'Davy'];
-//         $age =17;
-
-//         return $this->render('home_page/index.html.twig', [
-//             // 'controller_name' => 'HomePageController',
-//             'lesNoms' => $nomstudents,
-//             'age' => $age
-//         ]);
-//     }
-// }
 {
 #[Route('/', name: 'app_home_page', methods: ['GET'])]
     public function index(ProductRepository $productRepository, CategorieRepository $categorieRepository): Response
@@ -44,6 +31,28 @@ final class HomePageController extends AbstractController
             'product' => $product,
             'products' => $lastProductsAdd,
             'categories' => $categorieRepository->findAll()
+        ]);
+    }
+    #[Route('/product/subcategory/{id}/filter', name: 'app_home_product_filter', methods: ['GET'])]
+    public function filter(
+        int $id, 
+        AlphaCampRepository $alphaCampRepository, 
+        CategorieRepository $categorieRepository
+    ): Response {
+        // ici récupère la sous-catégorie
+        $alphaCamp = $alphaCampRepository->find($id);
+
+        if (!$alphaCamp) {
+            throw $this->createNotFoundException('Le secteur Alpha spécifié est introuvable.');
+        }
+
+        // Relation directe depuis l'objet AlphaCamp
+        $products = $alphaCamp->getProducts();
+
+        return $this->render('home_page/filter.html.twig', [
+            'products' => $products,
+            'categories' => $categorieRepository->findAll(),
+            'current_filter' => $alphaCamp->getName()
         ]);
     }
 }
