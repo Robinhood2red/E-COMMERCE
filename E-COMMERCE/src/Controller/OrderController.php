6c1fc1c4d2161943d609a12682bc2b5b00dd149e
Robinhood2red <louisboutetcamille@gmail.com>
2026-03-20 11:53:24 +0100

    vue

--- E-COMMERCE/src/Controller/OrderController.php
+++ E-COMMERCE/src/Controller/OrderController.php
@@ -7,7 +7,6 @@ use App\Entity\Order;
 use App\Form\OrderType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
