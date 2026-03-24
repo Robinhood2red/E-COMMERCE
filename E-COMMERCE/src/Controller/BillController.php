f60cf10993e06f7e8ac9af1165394d19c29e0e99
Robinhood2red <louisboutetcamille@gmail.com>
2026-03-24 16:09:16 +0100

    test pdf page complète

--- E-COMMERCE/src/Controller/BillController.php
+++ E-COMMERCE/src/Controller/BillController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,6 +26,42 @@ class BillController extends AbstractController
         return $this->render('bill/index.html.twig', [
             'order' => $order,
         ]);
+
+        //! ---------------------- ALTERNATIVE pour générer le pdf ---------------------------
+        // // Récupération de la commande
+        // $order = $orderRepository->find($id);
+
+        // if (!$order) {
+        //     throw $this->createNotFoundException("La commande demandée n'existe pas.");
+        // }
+
+        // // Définition des options PDF
+        // $pdfOptions = new Options();
+        // $pdfOptions->set('defaultFont', 'Arial'); // Définit la police par défaut
+
+        // // Initialisation de Dompdf
+        // $domPdf = new Dompdf($pdfOptions);
+
+        // // Génération du HTML à partir du template Twig
+        // $html = $this->renderView('bill/index.html.twig', [
+        //     'order' => $order,
+        // ]);
+
+        // // Chargement du HTML dans Dompdf
+        // $domPdf->loadHtml($html);
+
+        // // Création du rendu PDF
+        // $domPdf->render();
+
+        // // Envoi du PDF au navigateur
+        // $domPdf->stream('bill-' . $order->getId() . '.pdf', [
+        //     'Attachment' => false // Permet d'afficher le PDF dans le navigateur au lieu de le télécharger
+        // ]);
+
+        // return new Response('', 200, [
+        //     'Content-Type' => 'application/pdf'
+        // ]);
+        //! ---------------------- FIN ALTERNATIVE pour générer le pdf ---------------------------
     }
 }
     
